abb763b8a0315e494e14c90b81e5ce1840e5749d
Ruben civeira <rubenciveira@gmail.com>
2025-10-06 08:33:40 +0200

    chore: define index.html base path as relative

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async ({ mode }) => {
   return {
     plugins: [react()],
-
+    base: './',
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     //
     // 1. prevent Vite from obscuring rust errors
